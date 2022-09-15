846363b3611c52216ed35cc1c4f38e71fd4d33e2
Ong Jia Le <jiale.ong.2019@sis.smu.edu.sg>
2022-09-15 20:00:22 +0200

    feat-update-tablename-dummy-data

--- database/insert_dummy_data.sql
+++ database/insert_dummy_data.sql
@@ -1,7 +1,7 @@
 USE `govtech_url`;
 
 INSERT INTO `govtech_url` (`short_url`, `original_url`, `creator`, `click_count`) 
-VALUES ("googl", "www.google.com", "jiale", 10);
+VALUES ("googl", "http://www.google.com", "jiale", 10);
 
 INSERT INTO `govtech_url` (`short_url`, `original_url`, `creator`, `click_count`) 
-VALUES ("utub", "www.youtube.com", "jiale", 20);
+VALUES ("utub", "http://www.youtube.com", "jiale", 20);
